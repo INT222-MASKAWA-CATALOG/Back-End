af73fce7f907c6217cd849f375e7b551fe1ef7da
Possawat   Tanam <possawat.tanam@mail.kmutt.ac.th>
2021-08-20 00:10:48 +0700

    First Initial BackEnd

---
+++ Maskawa.Catalog/src/test/java/int222/integrated/Maskawa/Catalog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package int222.integrated.Maskawa.Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
