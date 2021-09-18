4ee476b27c71c69f3c629190e70f811c0973eb4e
Possawat   Tanam <possawat.tanam@mail.kmutt.ac.th>
2021-09-18 16:26:23 +0700

    [Delete] Bugs

--- src/main/java/int222/integrated/Application.java
+++ src/main/java/int222/integrated/Application.java
@@ -2,10 +2,6 @@ package int222.integrated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class Application {
