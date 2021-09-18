9aedcc54209b2b5f9222153843e1bd9961e3d5b6
Piyathida Thaneewan <piyathida.thaneewan@mail.kmutt.ac.th>
2021-09-18 16:15:32 +0700

    Add

--- src/main/java/int222/integrated/Application.java
+++ src/main/java/int222/integrated/Application.java
@@ -8,17 +8,10 @@ import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-//@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner init(StorageService storageService) {
-//		return (args) -> {
-//			storageService.init();
-//		};
-//	}
 }
